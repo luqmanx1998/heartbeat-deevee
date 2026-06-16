438c4dcc3a64b90dc6cc1c508f7eb3fe542d0e0a
luqmanx1998 <luqmanx1998@gmail.com>
2026-06-17 00:35:36 +0800

    test

--- app/lib/email/sendOrderEmail.js
+++ app/lib/email/sendOrderEmail.js
@@ -9,7 +9,7 @@ export async function sendOrderEmail({
   total,
   shippingAddress,
 }) {
-  const siteUrl = "https://heartbeat-deevee.vercel.app";
+  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL?.replace(/\/$/, "");
   const logoUrl = `${siteUrl}/deeveeemaildark2.png`;
   const heroUrl = `${siteUrl}/emailhero.jpg`;
 
